5a007d320cc8f3cae53bd0c089207120b11cbe85
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2021-12-21 18:19:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,13 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge_id
   belongs_to :dlivery_area_id
   belongs_to :delivery_days_id
+
+  validates :name, :version, :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_days_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
