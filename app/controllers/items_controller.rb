23083419c68e16e38ec2a07470c906b8ad856e1f
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2021-12-22 22:09:30 +0900

    エラー表示の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -10,19 +11,18 @@ class ItemsController < ApplicationController
   end
   
   def create
-    
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
+    end
   end
 
-end
+
 private
 
   def item_params
     params.require(:item).permit(:name,:version,:category_id,:situation_id,:delivery_charge_id,:delivery_area_id,:delivery_days_id,:price,:image).merge(user_id: current_user.id)
   end
-
 end
