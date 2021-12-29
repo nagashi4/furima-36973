69f9d554bbcadbe0b6fe12d83b803a12e5926c46
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2021-12-29 16:22:56 +0900

    修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_days_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   belongs_to :user
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
-  describe 'メッセージ投稿' do
-    context 'メッセージが投稿できない場合' do
+  describe '出品' do
+    context '出品できる場合' do
+      it '全ての項目が入力されていれば出品できる' do
+      expect(@item).to be_valid
+      end
+    end
+    context '出品できない場合' do
       it 'nameが空では登録できない' do
         @item.name = ''
         @item.valid?
@@ -56,6 +61,21 @@ RSpec.describe Item, type: :model do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it 'priceに半角英数位が含まれている場合は登録できない' do
+        @item.price = 'a'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが300未満では登録できない' do
+        @item.price = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが9999999以上では登録できない' do
+        @item.price = '10000000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
   end
