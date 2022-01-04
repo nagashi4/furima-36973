d533ac717b9ff5e1667bb0bd1458bf80af9b108f
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2022-01-04 18:42:36 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_days_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
 end
