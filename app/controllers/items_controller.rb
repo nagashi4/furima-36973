9bb5c29ea216692437268a922db614d76907de0a
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2022-01-08 18:05:40 +0900

    リダイレクト等の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -29,6 +30,11 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
+    if @item.save
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
 
@@ -38,4 +44,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :version, :category_id, :situation_id, :delivery_charge_id, :delivery_area_id,
                                  :delivery_days_id, :price, :image).merge(user_id: current_user.id)
   end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless   current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+  
 end
